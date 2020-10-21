3ef64e0bba66cbb94f6cfe2543ab6fd0b294ba97
Younas Hassani <Younas.HASSANI@sfpd.fgov.be>
2020-10-21 14:24:41 +0200

    hello

--- HelloWorld/src/be/abis/patterns/exercice00/test/ShapeTest.java
+++ HelloWorld/src/be/abis/patterns/exercice00/test/ShapeTest.java
@@ -13,17 +13,13 @@ public class ShapeTest {
     public void testShapeArea() {
         Rectangle rec = new Rectangle("blue",2,2);
         Circle cir = new Circle("green",5);
-        assertEquals (4,rec.area(),0);
-        assertEquals (78.54,round(cir.area()),0);
-        assertEquals ("blue",rec.getColor());
-        assertEquals ("green",cir.getColor());
 
         Shape[] shapeList = new Shape[2];
         shapeList[0] = rec;
         shapeList[1] = cir;
 
         assertEquals (4,shapeList[0] .area(),0);
-        assertEquals (78.53981633974483,shapeList[1].area(),0);
+        assertEquals (78.54,round(shapeList[1].area()),0);
         assertEquals ("blue",shapeList[0] .getColor());
         assertEquals ("green",shapeList[1].getColor());
     }
